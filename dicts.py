4325fea750ebf30c651e34432d64576cbac7c2b5
Diego Mejía <57368067+Yibizo@users.noreply.github.com>
2021-03-03 16:50:01 -0600

    Create dicts.py

---
+++ dicts.py
@@ -0,0 +1,11 @@
+ops = {
+    "+": lambda x,y: x+y,
+    "-": lambda x,y: x-y,
+    "*": lambda x,y: x*y,
+    "/": lambda x,y: x/y
+}
+
+programs = {
+    "example program": r"example\directory\",
+    "documents": r"D:"
+}
